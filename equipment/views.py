79cbfafc847dd79178b71dbbf0647d4917f06100
mphil17 <mattphil48@gmail.com>
2021-03-29 19:39:20 +0000

    Created equipment app and made an initial view.

--- equipment/views.py
+++ equipment/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Equipment
 
 # Create your views here.
+
+
+def show_equipment(request):
+
+    equipment = Equipment.objects.all()
+
+    context = {
+        'equipment': equipment,
+    }
+
+    return render(request, 'equipment/equipment.html', context)
