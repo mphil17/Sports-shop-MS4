36062ddb3dbb1fa589d4af41c9c280835082a271
mphil17 <mattphil48@gmail.com>
2021-04-03 17:52:14 +0000

    Added equipment individual item page and added functionality to search.

--- equipment/urls.py
+++ equipment/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.show_equipment, name='equipment')
+    path('', views.show_equipment, name='equipment'),
+    path('<item_id>', views.equipment_item, name='equipment_item'),
 ]
--- equipment/views.py
+++ equipment/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib import messages
+from django.db.models import Q
 from .models import Equipment
 
 # Create your views here.
@@ -7,9 +9,32 @@ from .models import Equipment
 def show_equipment(request):
 
     equipment = Equipment.objects.all()
+    query = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            query = request.GET['search']
+            if not query:
+                messages.error(request, "Please enter a search")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)| Q(condition__icontains=query)
+            equipment = equipment.filter(queries)
 
     context = {
         'equipment': equipment,
+        'search_term': query,
     }
 
     return render(request, 'equipment/equipment.html', context)
+
+
+def equipment_item(request, item_id):
+
+    item = get_object_or_404(Equipment, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'equipment/equipment_item.html', context)
