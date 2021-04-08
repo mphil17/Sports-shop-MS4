8459aa20b0fcdbf715bf90c24b4994897d849ad7
mphil17 <mattphil48@gmail.com>
2021-04-08 20:29:03 +0000

    Added form for adding to the shopping bag.

--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    print(request.session['bag'])
+    return redirect(redirect_url)
