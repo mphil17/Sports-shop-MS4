dbb8ec8d4f4f3508dda9f25c9c60d85ebe3d8518
mphil17 <mattphil48@gmail.com>
2021-04-10 16:59:26 +0000

    Added functionality to add to shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,13 +1,32 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from equipment.models import Equipment
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
     delivery = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+
+    print(bag)
+
+    for bag_item, quantity in bag.items():
+        equip_piece = get_object_or_404(Equipment, pk=bag_item)
+        total += quantity * equip_piece.price
+        product_count += quantity
+        delivery = 2
+        print(delivery)
+        print(total)
+        bag_items.append({
+            'bag_item': bag_item,
+            'equip_piece': equip_piece,
+            'quantity': quantity,
+        })
 
     grand_total = delivery + total
 
