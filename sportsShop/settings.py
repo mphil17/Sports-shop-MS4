fe8ec66b772885aaaffee9a4b634e6b31e270f50
mphil17 <mattphil48@gmail.com>
2021-04-13 20:49:55 +0000

    Added checkout app and models.

--- sportsShop/settings.py
+++ sportsShop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'equipment',
     'bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -73,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
         },
