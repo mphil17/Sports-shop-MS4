6856fe9071b1589146b44507ef954457bb4fd0df
mphil17 <mattphil48@gmail.com>
2021-04-17 16:40:43 +0000

    Added new secret_key and set debug.

--- sportsShop/settings.py
+++ sportsShop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n&(_z9&r822p&vgk=a3xmiih#o-*_c*xgh&3)8cuzhohfukywh'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Run debug only in development
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['sh-sports-shop', 'localhost']
 
