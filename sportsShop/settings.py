6fce31e8e649bf8b992752fbd728f17b570a62e4
mphil17 <mattphil48@gmail.com>
2021-04-22 20:15:04 +0000

    Added stripe.2

--- sportsShop/settings.py
+++ sportsShop/settings.py
@@ -168,6 +168,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'sh-sports-shop'
