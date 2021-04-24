66a5ae2b0735891171d669750f9a660eadea3e3b
mphil17 <mattphil48@gmail.com>
2021-04-24 17:00:26 +0000

    Coded webhooks in webhooks.py and webhook_handler.py. Added secret key for webhooks.

--- sportsShop/settings.py
+++ sportsShop/settings.py
@@ -172,6 +172,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
