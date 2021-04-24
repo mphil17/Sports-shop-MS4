4c90b44b06964f347b6bc29f2fb4c36c1d973254
mphil17 <mattphil48@gmail.com>
2021-04-24 20:36:02 +0000

    Completed Stripe payments and webhooks.

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,13 +12,13 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_id', 'date',
                        'delivery_cost', 'order_total',
-                       'grand_total',)
+                       'grand_total', 'original_bag', 'stripe_pid')
 
     fields = ('order_id', 'date', 'full_name',
               'email', 'tel_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'order_total', 'grand_total', 'original_bag', 'stripe_pid')
 
     list_display = ('order_id', 'date', 'full_name',
                     'order_total', 'delivery_cost',
